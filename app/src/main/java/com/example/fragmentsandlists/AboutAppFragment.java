7d566a78cd3fb1f83a4bf835bcc3523722611e00
Pavel <panteleevofficial@yandex.ru>
2021-07-31 10:06:54 +0300

    Homework for lesson 7

---
+++ app/src/main/java/com/example/fragmentsandlists/AboutAppFragment.java
@@ -0,0 +1,41 @@
+package com.example.fragmentsandlists;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class AboutAppFragment extends Fragment {
+
+    private final int firstArg = 1;
+    private final String secondArg = "ok";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_descriptions, container, false);
+        AboutAppInstance detail = AboutAppInstance.newInstance(firstArg, secondArg);
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.addToBackStack("previous");
+        fragmentTransaction.replace(R.id.side_menu_item_about_app, detail);
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.commit();
+        TextView aboutAppView = view.findViewById(R.id.textView);
+        aboutAppView.setText("Приложение с заметками");
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+}
